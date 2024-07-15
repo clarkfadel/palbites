28a0d72db936e2dc1dd80c45e312dfd643c3839a
clarkfadel <melyocraft09@gmail.com>
2024-07-16 04:14:21 +0800

    commit 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="logo.ico">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Greenviro</title>
+</head>
+<body>
+    <h1>The Palbites website is still being developed by <a href="https://ch1lo.com/">Ch1lo</a>.</h1>
+    
+    <img src="images/construction.svg" alt="Under Construction">
+
+</body>
+</html>
